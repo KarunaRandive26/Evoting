5e949427c7fc8b9128c5633e20dbe97ed089fc4d
Your Name <bajiraorandive9@gmail.com>
2023-10-07 17:42:27 +0530

    Initial import

---
+++ src/pages/AuthPage.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../pages/AuthPage.css";
+const AuthPage = () => {
+    return (
+        <>
+            <div className="loginpage row">
+                <div className="col-md-6 fill-a">
+                  
+                </div>
+                <div className="col-md-6 fill-b">
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default AuthPage;

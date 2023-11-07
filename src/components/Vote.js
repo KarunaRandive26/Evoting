600805fa217846eec3c8a3d07b9f1a6bbf79b5a2
Your Name <bajiraorandive9@gmail.com>
2023-11-07 15:36:55 +0530

    commit

--- src/components/Vote.js
+++ src/components/Vote.js
@@ -1,14 +1,31 @@
 import React from "react";
 import "./Sidebar.css";
 import "./Vote.css";
-import {NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Vote = () => {
     return (
         <>
             <div className="userpage votepage">
                 <div className="voteL-1">1. Choose the Election Assembly</div>
-                
-                <hr className="vline-1 col-11 m-auto"></hr>
+                <a className="consti p-3" href="/userpage/submit">
+                    <div className="asmb col-10 d-flex m-auto p-3">
+                        <div className="asmb-name col-11">Gram Panchayat</div>
+                        <div className="next-btn m-auto"><img className="sec-icon p-1" src="../images/next1.png" height="40px" width="40px"></img></div>
+                    </div>
+                </a>
+                <a className="consti p-3" href="/userpage/submit">
+                    <div className="asmb col-10 d-flex m-auto p-3">
+                        <div className="asmb-name col-11">Gram Panchayat</div>
+                        <div className="next-btn m-auto"><img className="sec-icon p-1" src="../images/next1.png" height="40px" width="40px"></img></div>
+                    </div>
+                </a>
+                <a className="consti p-3" href="/userpage/submit">
+                    <div className="asmb col-10 d-flex m-auto p-3">
+                        <div className="asmb-name col-11">Gram Panchayat</div>
+                        <div className="next-btn m-auto"><img className="sec-icon p-1" src="../images/next1.png" height="40px" width="40px"></img></div>
+                    </div>
+                </a>
+                <hr cla ssName="vline-1 col-11 m-auto"></hr>
                 <div class='walkthrough-pagination'>
                     <NavLink to="/userpage/vote" className='dot'></NavLink>
                     <NavLink to="/userpage/submit" className='dot'></NavLink>
--- src/pages/OtpPage.js
+++ src/pages/OtpPage.js
@@ -3,23 +3,26 @@ import "./OtpPage.css";
 
 const OtpPage = () => {
     useEffect(() => {
+        const otp=[];
         const codes = document.querySelectorAll('.code');
         codes[0].focus();
-
         codes.forEach((code, idx) => {
             code.addEventListener('keydown', (e) => {
                 if (e.key >= 0 && e.key <= 9) {
                     codes[idx].value = '';
+                    otp[idx]=e.key;
                     if (idx < codes.length - 1) {
                         setTimeout(() => codes[idx + 1].focus(), 10);
                     }
                 } else if (e.key === 'Backspace') {
                     if (idx > 0) {
                         setTimeout(() => codes[idx - 1].focus(), 10);
+                        otp[idx]=null;
                     }
                 }
             });
         });
+        console.log(otp);
     }, []);
 
     return (

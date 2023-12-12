57ab6ca7210a96483a8043889c3ee025498acfd6
Your Name <bajiraorandive9@gmail.com>
2023-12-12 12:21:54 +0530

    commit

--- src/components/AdminPage/Candidate.js
+++ src/components/AdminPage/Candidate.js
@@ -1,56 +1,58 @@
 import React from "react";
 import "./Candidate.css";
+import { NavLink } from "react-router-dom";
 
 const Candidate = () => {
     return (
         <>
-            <div className="d-flex">
-                <div className="Home backimage p-2">
-                    <div className="h1 home-title">Create Election</div>
-                    <div className="createpage">
-                        <div className="voteL-1">Add the Candidates </div>
-                        <div className="table-header d-flex my-2">
-                            <div className="col-2">Name Of the candidate</div>
-                            <div className="col-5">Party</div>
-                            <div className="col-2">Symbol</div>
-                            <div className="col-3">Vote</div>
-                        </div>
-                        <div className="table-data d-flex ">
-                            <div className="col-2 m-auto">
-                                <div className="cad-name">Narendra Modi</div>
-                            </div>
-                            <div className="col-5 m-auto">Bharatiya Janata Party</div>
-                            <div className="col-2 m-auto"><img className="" src="../images/logo1.png" ></img></div>
-                            <div className="col-3 "><a className="vote-btn-a" href="/submitvote"><div className="btn-sm add-btn col-4 m-auto">Add</div></a></div>
-                        </div>
-                        <hr className="vline-1 col-11 m-auto"></hr>
-                        <div className="table-data d-flex ">
-                            <div className="col-2 m-auto">
-                                <div className="cad-name">Narendra Modi</div>
-                            </div>
-                            <div className="col-5 m-auto">Bharatiya Janata Party</div>
-                            <div className="col-2 m-auto"><img className="" src="../images/logo1.png" ></img></div>
-                            <div className="col-3 m-auto"><a className="vote-btn-a" href="/submitvote"><div className="btn-sm add-btn col-4 m-auto">Add</div></a></div>
-                        </div>
-                        <hr className="vline-1 col-11 m-auto"></hr>
-                        <div className="table-data d-flex ">
-                            <div className="col-2 m-auto">
-                                <div className="cad-name">Narendra Modi</div>
-                            </div>
-                            <div className="col-5 m-auto">Bharatiya Janata Party</div>
-                            <div className="col-2 m-auto"><img className="" src="../images/logo1.png" ></img></div>
-                            <div className="col-3 m-auto"><a className="vote-btn-a" href="/submitvote"><div className="btn-sm add-btn col-4 m-auto">Add</div></a></div>
-                        </div>
-                        <hr className="vline-1 col-11 m-auto"></hr>
-                        <div className="table-data d-flex ">
-                            <div className="col-2 m-auto">
-                                <div className="cad-name">Narendra Modi</div>
-                            </div>
-                            <div className="col-5 m-auto">Bharatiya Janata Party</div>
-                            <div className="col-2 m-auto"><img className="" src="../images/logo1.png" ></img></div>
-                            <div className="col-3 m-auto"><a className="vote-btn-a" href="/submitvote"><div className="btn-sm add-btn col-4 m-auto">Add</div></a></div>
-                        </div>
+            <div className="adminpage">
+                <div className="voteL-1">Add Candidates</div>
+                <div className="table-header d-flex my-2">
+                    <div className="col-2">Select</div>
+                    <div className="col-3">Name Of the candidate</div>
+                    <div className="col-5">Party</div>
+                    <div className="col-2">Symbol</div>
+                </div>
+                <div className="table-data d-flex ">
+                    <div className="col-2"><input className="cad-check" type="checkbox"></input></div>
+                    <div className="col-3 m-auto">
+                        <div className="cad-name">Narendra Modi</div>
                     </div>
+                    <div className="col-5 m-auto">Bharatiya Janata Party</div>
+                    <div className="col-2 m-auto"><img className="" src="../images/logo1.png" ></img></div>
+                </div>
+                <hr className="vline-1 col-11 m-auto"></hr>
+                <div className="table-data d-flex ">
+                <div className="col-2"><input className="cad-check" type="checkbox"></input></div>
+                    <div className="col-3 m-auto">
+                        <div className="cad-name">Narendra Modi</div>
+                    </div>
+                    <div className="col-5 m-auto">Bharatiya Janata Party</div>
+                    <div className="col-2 m-auto"><img className="" src="../images/logo1.png" ></img></div>
+                </div>
+                <hr className="vline-1 col-11 m-auto"></hr>
+                <div className="table-data d-flex ">
+                <div className="col-2"><input className="cad-check" type="checkbox"></input></div>
+                    <div className="col-3 m-auto">
+                        <div className="cad-name">Narendra Modi</div>
+                    </div>
+                    <div className="col-5 m-auto">Bharatiya Janata Party</div>
+                    <div className="col-2 m-auto"><img className="" src="../images/logo1.png" ></img></div>
+                </div>
+                <hr className="vline-1 col-11 m-auto"></hr>
+                <div className="table-data d-flex ">
+                <div className="col-2"><input className="cad-check" type="checkbox"></input></div>
+                    <div className="col-3 m-auto">
+                        <div className="cad-name">Narendra Modi</div>
+                    </div>
+                    <div className="col-5 m-auto">Bharatiya Janata Party</div>
+                    <div className="col-2 m-auto"><img className="" src="../images/logo1.png" ></img></div>
+                </div>
+                <hr className="vline-1 col-11 m-auto"></hr>
+                <div class='walkthrough-pagination'>
+                    <NavLink to="/userpage/vote" className='dot'></NavLink>
+                    <NavLink to="/userpage/voteboard" className='dot'></NavLink>
+                    <NavLink to="/userpage/submit" className='dot'></NavLink>
                 </div>
             </div>
         </>
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
 import Sidebar from "../components/Sidebar";
@@ -8,27 +8,29 @@ import SubmitVote from "../components/SubmitVote";
 import Guidelines from "../components/Guidelines";
 import Voteboard from "../components/Voteboard";
 const UserPage = () => {
-    return (
-        <>
-            <div className="userpage d-flex">
-                <Sidebar/>
-                <div className="mainpage col-10">
-                    <div className="content">
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/dashboard" element={<Home/>}/>
-                        <Route path="/guidelines" element={<Guidelines/>}/>
-                        <Route path="/vote" element={<Vote/>}/>
-                        <Route path="/voteboard" element={<Voteboard/>}/>
-                        <Route path="/submit" element={<SubmitVote/>}/>
-                        <Route path="/contact" element={<Contact/>}/>
-                        <Route path="/result" element={<Home/>}/>
-                    </Routes>
+    
+        return (
+            <>
+                <div className="userpage d-flex">
+                    <Sidebar />
+
+                    <div className="mainpage col-10">
+                        <div className="content">
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/dashboard" element={<Home />} />
+                                <Route path="/guidelines" element={<Guidelines />} />
+                                <Route path="/vote" element={<Vote />} />
+                                <Route path="/voteboard" element={<Voteboard />} />
+                                <Route path="/submit" element={<SubmitVote />} />
+                                <Route path="/contact" element={<Contact />} />
+                                <Route path="/result" element={<Home />} />
+                            </Routes>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </>
-    );
-};
+            </>
+        );
+    };
 
 export default UserPage;
